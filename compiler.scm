081b31e91453c2239704ef6d7c41ff952c2144d9
YuvalHZ <yuval_hz@hotmail.com>
2016-11-27 08:56:30 -0800

    more parsers

--- compiler.scm
+++ compiler.scm
@@ -10,7 +10,7 @@
   (range #\1 #\9))
 
 ;natural number
-(define <nat>
+(define <Natural>
   (new (*parser (char #\0))
        (*pack (lambda (_) 0))
 
@@ -27,35 +27,41 @@
        done))
 
 ;integers
-(define <int>
+(define <Integer>
   (new (*parser (char #\+))
-       (*parser <nat>)
+       (*parser <Natural>)
        (*caten 2)
        (*pack-with
          (lambda (++ n) n))
 
        (*parser (char #\-))
-       (*parser <nat>)
+       (*parser <Natural>)
        (*caten 2)
        (*pack-with
          (lambda (-- n) (- n)))
 
-       (*parser <nat>)
+       (*parser <Natural>)
 
        (*disj 3)
 
        done))
 
-;rational numbers
-(define <rat>
-  (new (*parser <int>)
+;rational numbers (Fraction)
+(define <Fraction>
+  (new (*parser <Integer>)
        (*parser (char #\/))
-       (*parser <nat>)
+       (*parser <Natural>)
        (*guard (lambda (n) (not (zero? n))))
        (*caten 3)
        (*pack-with
  (lambda (num div den)
    (/ num den)))
+       done))
+       
+(define <Number>
+  (new (*parser <Integer>)
+       (*parser <Fraction>)
+       (*disj 2)
        done))
 
 ;hex digit
@@ -100,7 +106,7 @@
    (+ l (* 256 h))))
        done))
 
-(define <hex-char>
+(define <HexChar>
   (new (*parser (word-ci "\\{0x"))
 
        (*parser <XXXX>)
@@ -112,33 +118,40 @@
        (*caten 3)
        (*pack-with (lambda (_< ch _>) ch))
        done))
+       
+(define <StringHexChar>
+  (new (*parser (word-ci "\\x"))
+       (*parser <HexChar>) *star
+       ;TODO add pack with
+       (*caten 2)
+       done))
 
 ;make string meta char
-(define ^<meta-char>
+(define ^<MetaChar>
   (lambda (str ch)
     (new (*parser (word str))
   (*pack (lambda (_) ch))
   done)))
 
 ;string meta char
-(define <string-meta-char>
-  (new (*parser <hex-char>)
-       (*parser (^<meta-char> "\\\\" #\\))
-       (*parser (^<meta-char> "\\\"" #\"))
-       (*parser (^<meta-char> "\\n" #\newline))
-       (*parser (^<meta-char> "\\r" #\return))
-       (*parser (^<meta-char> "\\t" #\tab))
-       (*parser (^<meta-char> "\\f" #\page)) ; formfeed
-       (*parser (^<meta-char> "\\{lambda}" (integer->char 955)))
-       (*parser (^<meta-char> "\\{alef}" (integer->char 1488)))
-       (*parser (^<meta-char> "\\{bismillah}" (integer->char 65021)))
-       (*parser (^<meta-char> "\\{smiley}" (integer->char 9786)))
+(define <StringMetaChar>
+  (new (*parser (^<MetaChar> "\\\\" #\\))
+       (*parser (^<MetaChar> "\\\"" #\"))
+       (*parser (^<MetaChar> "\\n" #\newline))
+       (*parser (^<MetaChar> "\\r" #\return))
+       (*parser (^<MetaChar> "\\t" #\tab))
+       (*parser (^<MetaChar> "\\f" #\page)) ; formfeed
+       (*parser (^<MetaChar> "\\{lambda}" (integer->char 955)))
+       (*parser (^<MetaChar> "\\{alef}" (integer->char 1488)))
+       (*parser (^<MetaChar> "\\{bismillah}" (integer->char 65021)))
+       (*parser (^<MetaChar> "\\{smiley}" (integer->char 9786)))
 
        (*disj 11)
        done))
+       
 ;string char
-(define <string-char>
-  (new (*parser <string-meta-char>)
+(define <StringChar>
+  (new (*parser <StringMetaChar>)
 
        (*parser <any-char>)
 
@@ -149,11 +162,17 @@
        *diff
        (*disj 2)
        done))
+       
+(define <Symbol>
+  (new (*paser <StringChar>)
+       (*paser <StringChar>) *star
+       (*caten 2)
+       done))
 
 ;string
-(define <string>
+(define <String>
   (new (*parser (char #\"))
-       (*parser <string-char>) *star
+       (*parser <StringChar>) *star
        (*parser (char #\"))
        (*caten 3)
 
@@ -164,7 +183,7 @@
        done))
 
 ;boolean
-(define <boolean>
+(define <Boolean>
   (new (*parser (char #\#))
        
        (*parser (char #\t))
@@ -178,3 +197,121 @@
 	 '(,ht ,@pred))))
 	done))
        
+;InfixPrefixExtensionPrefix
+(define <InfixPrefixExtensionPrefix>
+  (new (*parser (char #\#))
+       
+       (*parser (char #\#))
+       (*parser (char #\%))
+       (*disj 2)
+       
+       (*caten 2)
+       (*pack-with
+	(lambda (...)))
+	
+	done))
+	
+;PowerSymbol
+(define <PowerSymbol>
+  (new (*parser (char #\*))
+       (*parser (char #\*))
+       (*caten 2)
+       
+       (*parser (char #\^))
+       
+       (*disj 2)
+       (*pack-with
+	(lambda (...)))
+	
+	done))
+
+(define <InfixAdd>
+  (new (*parser <Number>)
+       (*parser (char #\+))
+       (*parser <InfixExpression>)
+       (*caten 3)
+       (*pack-with
+	(lambda (num add expression)
+	  (+ num expression)))
+       done))
+       
+(define <InfixNeg>
+  (new (*parser (char #\-))
+       (*parser <InfixExpression>)
+       (*caten 2)
+       (*pack-with
+	(lambda (neg num)
+	  (- num)))
+       done))
+	
+(define <InfixSub>
+  (new (*parser <Number>)
+       (*parser (char #\-))
+       (*parser <InfixExpression>)
+       (*caten 3)
+       (*pack-with
+	(lambda (num sub expression)
+	  (- num expression)))
+       done))
+       
+(define <InfixMul>
+  (new (*parser <Number>)
+       (*parser (char #\*))
+       (*parser <InfixExpression>)
+       (*pack-with
+	(lambda (num mul expression)
+	  (* num expression)))
+       done))
+	
+(define <InfixDiv>
+  (new (*parser <Number>)
+       (*parser (char #\/))
+       (*parser <InfixExpression>)
+       (*guard (lambda (n) (not (zero? n))))
+       (*pack-with
+	(lambda (num div expression)
+	  (/ num expression)))
+       done))
+       
+(define <InfixPow>
+  (new (*parser <Number>)
+       (*parser <PowerSymbol>)
+       (*parser <InfixExpression>)
+       (*pack-with
+	(lambda (num pow expression)
+	  (^ num expression)))
+       done))
+       
+(define <action-symbol>
+  (new (*parser (char #\+))
+       (*parser (char #\-))
+       (*parser (char #\*))
+       (*parser <PowerSymbol>)
+       (*parser (char #\/))
+       (*disj 5)
+       done))
+       
+(define <InfixSymbol>
+  (new (*parser <Symbol>)
+       (*parser <action-symbol>)
+       *diff
+       done))
+	
+(define <InfixExpression>
+  (new (*parser <InfixAdd>)
+       (*parser <InfixNeg>)
+       (*parser <InfixSub>)
+       (*parser <InfixMul>)
+       (*parser <InfixDiv>)
+       (*parser <InfixPow>)
+       (*parser <InfixArrayGet>)
+       (*parser <InfixFuncall>)
+       (*parser <InfixParen>)
+       (*parser <InfixSexprEscape>)
+       (*parser <InfixSymbol>)
+       (*parser <Number>)
+       
+       (*disj 12)
+  done))
+ 
+ 
