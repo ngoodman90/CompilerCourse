2dba9ba355536725b665da85a2d5b35b729b08fc
YuvalHZ <yuval_hz@hotmail.com>
2017-03-17 02:10:53 +0200

    new code

--- final-project/test-my-project/SchemePrims.scm
+++ final-project/test-my-project/SchemePrims.scm
@@ -1,108 +1,115 @@
+; ############################# not #############################
+(define not
+    (lambda (x)
+        (if x #f #t)))
+
+; ############################# list #############################
+    
+(define list
+    (lambda x x))
+    
+; ############################# map #############################
+    
 (define map
-  ((lambda (y) 
-     ((lambda (map1) 
-	((lambda (maplist) 
-	   (lambda (f . s) 
-	     (maplist f s))) 
-	 (y (lambda (maplist) 
-	      (lambda (f s) 
-		(if (null? (car s)) '() 
-		    (cons (apply f (map1 car s)) 
-			  (maplist f (map1 cdr s))))))))) 
-      (y (lambda (map1) 
-	   (lambda (f s) 
-	     (if (null? s) '() 
-		 (cons (f (car s)) 
-		       (map1 f (cdr s))))))))) 
-   (lambda (f) 
-     ((lambda (x) 
-	(f (lambda (y z)
-	     ((x x) y z))))
-      (lambda (x) 
-	(f (lambda (y z)
-	     ((x x) y z))))))))
-            
-            
-(define list (lambda ls ls))             
+    ((lambda (a) 
+        ((lambda (m) 
+            ((lambda (m-lst) 
+                (lambda (f . b) 
+                    (m-lst f b))) 
+            (a (lambda (m-lst) 
+                (lambda (f b) 
+                    (if (null? (car b)) '() 
+                        (cons (apply f (m car b)) 
+                            (m-lst f (m cdr b))))))))) 
+        (a (lambda (m) 
+            (lambda (f b) 
+                (if (null? b) '() 
+                    (cons (f (car b)) 
+                        (m f (cdr b)))))))))
+    (lambda (f)
+        ((lambda (x)
+            (f (lambda (a y)
+                ((x x) a y))))
+        (lambda (x) 
+            (f (lambda (a y)
+                ((x x) a y))))))))
 
-
- 
- 
-(define not (lambda (x) (if x #f #t)))
+; ############################# foldr #############################
 
 (define foldr
-  (lambda (binop final s) 
-  'x
-    (letrec ((loop
-	      (lambda (s)
-		(if (null? s) final
-		    (binop (car s) (loop (cdr s)))))))
-      (loop s))))
-      
-      
-;; (define (append . lsts)
-;;   (foldr (lambda (sublist acc)
-;;            (appendBinary sublist acc))
-;;          '()
-;;          lsts))
-;;          
-;; (define (appendBinary lis1 lis2)
-;;   (cond ((null? lis1)
-;;          lis2)
-;;         (else
-;;          (cons (car lis1)
-;;                (appendBinary (cdr lis1) lis2)))))
-            
-(define append
-  (letrec ((app2
-	    (lambda (s1 s2)
-	      (if (null? s1) s2
-		  (cons (car s1)
-		   (app2 (cdr s1) s2)))))
-	   (appl
-	    (lambda (s1 s)
-	      (if (null? s) s1
-		  (app2 s1 (appl (car s) (cdr s)))))))
-    (lambda s
-      (if (null? s) '()
-	  (appl (car s) (cdr s))))))
-	  
-	  
-               
+    (lambda (op last a)
+        'a
+        (letrec ((loop
+                    (lambda (x)
+                        (if (not (null? x))
+                            (op (car x) (loop (cdr x)))
+                            last))))
+            (loop a))))
 
-(define compose
-  (let ((binary-compose
-	 (lambda (f g)
-	   (lambda (x)
-	     (f (g x))))))
-    (lambda s
-      (foldr binary-compose (lambda (x) x) s))))
+; ############################# append #############################
+
+(define help-concat
+    (lambda (a b)
+        (if (not (null? a))
+            (cons (car a) (help-concat (cdr a) b))
+            b)))
+            
+(define concat
+    (lambda (a lst)
+        (if (not (null? lst))
+            (help-concat a (concat (car lst) (cdr lst)))
+            a)))
+
+(define append
+    (lambda lst
+        (if (null? lst)
+            '()
+            (concat (car lst) (cdr lst)))))
+
+; ############################# car cdr #############################
+
+(define special-func1
+    (lambda (f g)
+        (lambda (x)
+            (f (g x)))))
+
+(define special-func
+    (lambda a
+        (foldr
+            special-func1
+            (lambda (x) x)
+            a)))
       
-(define caar (compose car car))
-(define cadr (compose car cdr))
-(define cdar (compose cdr car))
-(define cddr (compose cdr cdr))
-(define caaar (compose car caar))
-(define caadr (compose car cadr))
-(define cadar (compose car cdar))
-(define caddr (compose car cddr))
-(define cdaar (compose cdr caar))
-(define cdadr (compose cdr cadr))
-(define cddar (compose cdr cdar))
-(define cdddr (compose cdr cddr))
-(define caaaar (compose car caaar))
-(define caaadr (compose car caadr))
-(define caadar (compose car cadar))
-(define caaddr (compose car caddr))
-(define cadaar (compose car cdaar))
-(define cadadr (compose car cdadr))
-(define caddar (compose car cddar))
-(define cadddr (compose car cdddr))
-(define cdaaar (compose cdr caaar))
-(define cdaadr (compose cdr caadr))
-(define cdadar (compose cdr cadar))
-(define cdaddr (compose cdr caddr))
-(define cddaar (compose cdr cdaar))
-(define cddadr (compose cdr cdadr))
-(define cdddar (compose cdr cddar))
-(define cddddr (compose cdr cdddr))
+(define caar (special-func car car))
+(define cadr (special-func car cdr))
+
+(define caaar (special-func car caar))
+(define caadr (special-func car cadr))
+(define cadar (special-func car cdar))
+(define caddr (special-func car cddr))
+
+(define caaaar (special-func car caaar))
+(define caaadr (special-func car caadr))
+(define caadar (special-func car cadar))
+(define caaddr (special-func car caddr))
+(define cadaar (special-func car cdaar))
+(define cadadr (special-func car cdadr))
+(define caddar (special-func car cddar))
+(define cadddr (special-func car cdddr))
+
+(define cdar (special-func cdr car))
+(define cddr (special-func cdr cdr))
+
+(define cdaar (special-func cdr caar))
+(define cdadr (special-func cdr cadr))
+(define cddar (special-func cdr cdar))
+(define cdddr (special-func cdr cddr))
+
+(define cdaaar (special-func cdr caaar))
+(define cdaadr (special-func cdr caadr))
+(define cdadar (special-func cdr cadar))
+(define cdaddr (special-func cdr caddr))
+(define cddaar (special-func cdr cdaar))
+(define cddadr (special-func cdr cdadr))
+(define cdddar (special-func cdr cddar))
+(define cddddr (special-func cdr cdddr))
